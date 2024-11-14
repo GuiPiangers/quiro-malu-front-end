3d2980b55508e0f5a3f2841521cd6cc584e3e7f3
Guilherme <eduardomartinspiangers@gmail.com>
2024-11-14 13:22:49 +0000

    remove shcduling calendar for tests

--- src/app/(private)/scheduling/layout.tsx
+++ src/app/(private)/scheduling/layout.tsx
@@ -3,8 +3,6 @@ import { Viewport } from 'next'
 export const viewport: Viewport = {
   width: 'device-width',
   initialScale: 1,
-  maximumScale: 1,
-  userScalable: false,
   viewportFit: 'cover',
 }
 export default function SchedulingLayout({
