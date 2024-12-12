f2748ebe45a84964d77865a8bfc38ee612cc42fd
Guilherme <eduardomartinspiangers@gmail.com>
2024-12-12 19:57:49 +0000

    refactor: change SchedulingModal component to use SchedulingModalContent component

--- src/components/modal/SchedulingModal/SchedulingModal.tsx
+++ src/components/modal/SchedulingModal/SchedulingModal.tsx
@@ -1,16 +1,10 @@
 'use client'
 
-import Modal, { ModalHandles } from '@/components/modal/Modal'
+import { ModalHandles } from '@/components/modal/Modal'
 import { ReactNode, useRef } from 'react'
-import SchedulingForm from '../../../app/(private)/scheduling/components/SchedulingForm'
 import { SchedulingResponse } from '@/services/scheduling/actions/scheduling'
-import { responseError } from '@/services/api/api'
-import HeaderForm from '@/components/modal/HeaderModal'
-import { useRouter } from 'next/navigation'
 import Button, { ButtonPropsVariants } from '@/components/Button'
-import { SchedulingStatusEnum } from '@/services/scheduling/actions/schedulingStatusEnum'
-import { useUpdateScheduling } from '@/hooks/scheduling/useUpdateScheduling'
-import { useCreateScheduling } from '@/hooks/scheduling/useCreateScheduling'
+import SchedulingModalContent from './SchedulingModalContent'
 
 export default function SchedulingModal({
   color,
@@ -26,45 +20,14 @@ export default function SchedulingModal({
   >
 } & ButtonPropsVariants) {
   const modalRef = useRef<ModalHandles>(null)
-  const router = useRouter()
-  const updateScheduling = useUpdateScheduling()
-  const createScheduling = useCreateScheduling()
-
   const openModal = () => modalRef.current?.openModal()
-  const closeModal = () => modalRef.current?.closeModal()
-
-  const afterSubmit = () => {
-    closeModal()
-    router.refresh()
-  }
-
-  const formAction = async (
-    data: SchedulingResponse & { patient: string; phone: string },
-  ): Promise<SchedulingResponse | responseError> => {
-    if (formData?.id) return await updateScheduling.mutateAsync(data)
-
-    const result = await createScheduling.mutateAsync(data)
-    console.log('resultado ', result)
-    return await createScheduling.mutateAsync(data)
-  }
 
   return (
     <>
       <Button color={color || 'green'} {...props} onClick={openModal}>
         {children}
       </Button>
-      <Modal ref={modalRef} className="m-4 w-full max-w-md p-0">
-        <HeaderForm
-          handleClose={closeModal}
-          title={formData?.id ? 'Editar agendamento' : 'Novo agendamento'}
-        />
-        <SchedulingForm
-          formData={{ status: SchedulingStatusEnum.scheduled, ...formData }}
-          className="shadow-none"
-          action={formAction}
-          afterValidation={afterSubmit}
-        />
-      </Modal>
+      <SchedulingModalContent ref={modalRef} formData={formData} />
     </>
   )
 }
