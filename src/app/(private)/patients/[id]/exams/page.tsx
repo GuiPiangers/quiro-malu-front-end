587ee95db80a965b0800860e846913afbc366890
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2025-01-18 13:51:46 -0300

    feat: create patient exam page exams list

--- src/app/(private)/patients/[id]/exams/page.tsx
+++ src/app/(private)/patients/[id]/exams/page.tsx
@@ -1,3 +1,35 @@
-export default function Exams() {
-  return <div></div>
+import Button from '@/components/Button'
+import ExamFile from '@/components/examFile/ExamFile'
+import { listExams, saveExam } from '@/services/exam/exam'
+import { ParamsType } from '../page'
+import { Validate } from '@/services/api/Validate'
+import { decode } from 'utf8'
+import { Box } from '@/components/box/Box'
+
+export default async function Exams({ params }: { params: ParamsType }) {
+  const patientId = params.id
+  const exams = await listExams({ patientId, page: 1 })
+
+  return (
+    <section>
+      <Box className="flex flex-col gap-4">
+        <div className="flex flex-wrap gap-2">
+          {Validate.isOk(exams) &&
+            exams.map((exam) => (
+              <ExamFile
+                key={exam.id}
+                fileUrl={exam.url}
+                fileName={decode(exam.fileName)}
+              />
+            ))}
+          {/* <ExamFile fileUrl="/" fileName="quiro-exame.pdf" /> */}
+        </div>
+        <form action={saveExam.bind(null, patientId)}>
+          <label htmlFor="exam-input"></label>
+          <input type="file" name="file" id="exam-input"></input>
+          <Button>Enviar</Button>
+        </form>
+      </Box>
+    </section>
+  )
 }
