ffde5455d2534e27f3103b58a6f03c5c0117d0f8
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2025-01-18 21:40:30 -0300

    feat: create ExamFile component

---
+++ src/components/exam/ExamFile.tsx
@@ -0,0 +1,26 @@
+import { IoDocumentTextOutline } from 'react-icons/io5'
+
+type ExamFileProps = {
+  fileUrl: string
+  fileName: string
+}
+
+export default function ExamFile({ fileUrl, fileName }: ExamFileProps) {
+  return (
+    <a
+      className="flex  flex-col items-center gap-2"
+      href={fileUrl}
+      target="_blank"
+    >
+      <div className="group flex h-24 w-24 items-center justify-center rounded-md border border-main hover:border-main-hover">
+        <IoDocumentTextOutline
+          size={28}
+          className="text-main group-hover:text-main-hover"
+        />
+      </div>
+      <span className="text-md relative max-w-[172px] overflow-hidden overflow-ellipsis whitespace-nowrap">
+        {fileName}
+      </span>
+    </a>
+  )
+}
