dfe7496eeac1834f2ed109d06951d6dad4bb11bb
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2025-01-25 13:28:10 -0300

    fix: update service page to list patients with search filter

--- src/app/(private)/services/page.tsx
+++ src/app/(private)/services/page.tsx
@@ -5,12 +5,13 @@ import { listService } from '@/services/service/Service'
 export default async function Services({
   searchParams,
 }: {
-  searchParams: { page?: string }
+  searchParams: { page?: string; pesquisa?: string }
 }) {
   const page =
     searchParams.page && +searchParams.page > 0 ? searchParams.page : '1'
+  const search = searchParams.pesquisa ? searchParams.pesquisa : ''
 
-  const res = await listService({ page }).then((data) =>
+  const res = await listService({ page, search }).then((data) =>
     Validate.isOk(data) ? data : undefined,
   )
 
