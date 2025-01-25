ff523a1dc8443041938c4e62130b89190c31b187
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2025-01-24 23:11:28 -0300

    fix: update loading patient page to don't use SearchInput

--- src/app/(private)/patients/loading.tsx
+++ src/app/(private)/patients/loading.tsx
@@ -1,14 +1,21 @@
-import SearchInput from '@/components/input/SearchInput'
 import TableSkeleton from '@/components/skeleton/TableSkeleton'
 import { Box } from '@/components/box/Box'
 import Button from '@/components/Button'
 import Link from 'next/link'
+import { Input } from '@/components/input'
+import { IoIosSearch } from 'react-icons/io'
 
 export default function loadingPatients() {
   return (
     <Box className="w-full max-w-screen-lg">
       <div className="mb-6 grid grid-cols-1 items-center gap-4 sm:grid-cols-[1fr_auto]">
-        <SearchInput className="text-base" />
+        <Input.Root>
+          <Input.Field
+            placeholder="Pesquisar..."
+            autoComplete="off"
+            startAdornment={<IoIosSearch size={24} className="ml-3" />}
+          ></Input.Field>
+        </Input.Root>
         <Button asChild color="green">
           <Link href="/patients/create">Cadastrar</Link>
         </Button>
