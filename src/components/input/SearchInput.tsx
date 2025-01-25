99042ff6c5c4a55922f276f90545e9a50d0fa0be
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2025-01-24 22:49:33 -0300

    feat: update SearchInput component to update searchParams with searched value

--- src/components/input/SearchInput.tsx
+++ src/components/input/SearchInput.tsx
@@ -1,14 +1,36 @@
+'use client'
+
 import { IoIosSearch } from 'react-icons/io'
 import { Input } from '.'
+import { useRouter, useSearchParams } from 'next/navigation'
+import { generateSearchParams } from '@/utils/generateSearchParams'
+import { convertEntriesToObject } from '@/utils/convertEntriesToObject'
 
 type SearchInputProps = {
   className?: string
+  search?: {
+    param: string
+  }
 }
 
-export default function SearchInput({ className }: SearchInputProps) {
+export default function SearchInput({ className, search }: SearchInputProps) {
+  const route = useRouter()
+  const searchParams = useSearchParams()
+  const searchParamsObject = convertEntriesToObject(
+    Array.from(searchParams.entries()),
+  )
+
   return (
     <Input.Root>
       <Input.Field
+        onChange={(e) => {
+          if (!search) return
+          const params = generateSearchParams({
+            ...searchParamsObject,
+            [search.param]: e.target.value,
+          })
+          route.push(params)
+        }}
         className={className}
         placeholder="Pesquisar..."
         autoComplete="off"
