faf0b3b3061d02a0da61564281b33e5bcd093bb9
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2025-02-09 19:45:42 -0300

    feat: create useWebPush notification to registry service worker and subscribe notification on backend

---
+++ src/hooks/useWebPush.ts
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react'
+
+const VAPID_PUBLIC_KEY = process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY
+
+export function useNotifications() {
+  const [permission, setPermission] =
+    useState<NotificationPermission>('default')
+
+  useEffect(() => {
+    if ('Notification' in window && Notification.permission === 'default') {
+      Notification.requestPermission().then(setPermission)
+    }
+  }, [])
+
+  const subscribeToPush = async () => {
+    if (!('serviceWorker' in navigator) || !('PushManager' in window)) {
+      return
+    }
+
+    const registration = await navigator.serviceWorker.register('/sw.js')
+
+    const subscription = await registration.pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey: urlBase64ToUint8Array(VAPID_PUBLIC_KEY!),
+    })
+
+    await fetch('http://localhost:8000/subscribe', {
+      method: 'POST',
+      body: JSON.stringify(subscription),
+      headers: { 'Content-Type': 'application/json' },
+    })
+  }
+
+  return { subscribeToPush, permission }
+}
+
+function urlBase64ToUint8Array(base64String: string) {
+  const padding = '='.repeat((4 - (base64String.length % 4)) % 4)
+  const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/')
+
+  const rawData = window.atob(base64)
+  const outputArray = new Uint8Array(rawData.length)
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i)
+  }
+  return outputArray
+}
