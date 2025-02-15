3f484e8e26906c37835c45f01e145c236952a7bd
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2025-02-15 15:49:56 -0300

    feat: add click notification listener to service worker

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,25 @@
+/* global clients */
+
 self.addEventListener('push', (e) => {
   const data = e.data.json()
   self.registration.showNotification(data.title, {
     body: data.body,
   })
+})
+
+self.addEventListener('notificationclick', (e) => {
+  e.notification.close()
+
+  e.waitUntil(
+    clients
+      .matchAll({ type: 'window', includeUncontrolled: true })
+      .then((clientList) => {
+        for (const client of clientList) {
+          if (client.visibilityState === 'visible') {
+            return client.focus()
+          }
+        }
+        return clients.openWindow('/')
+      }),
+  )
 })
