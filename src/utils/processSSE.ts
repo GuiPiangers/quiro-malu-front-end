eacadc7303a2d73db054a69ea3a59c3662b8ebbe
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2025-02-22 14:02:38 -0300

    feat: implement processSSE class

--- src/utils/processSSE.ts
+++ src/utils/processSSE.ts
@@ -1,38 +1,39 @@
-type Fetch = (
-  input: RequestInfo,
-  init?: RequestInit & { noContentType?: boolean },
-) => Promise<Response>
-
 export class ProcessSEE {
   private dataStream?: ReadableStream<unknown>
+  private abortController: AbortController | null = null
 
-  async connect(
-    fetchFn: Fetch,
-    input: RequestInfo,
-    init?: RequestInit & { noContentType?: boolean },
-  ) {
-    const data = await fetchFn(input, init)
+  async connect(url: string, init: RequestInit) {
+    try {
+      if (this.abortController) return
+      this.abortController = new AbortController()
+      const data = await fetch(url, {
+        signal: this.abortController.signal,
+        ...init,
+      })
 
-    if (!data.ok || !data.body) {
-      console.error('Falha ao conectar ao SSE')
-      return
+      if (!data.ok || !data.body) {
+        console.error('Falha ao conectar ao SSE')
+        return
+      }
+
+      const stream = data.body
+        ?.pipeThrough(new TextDecoderStream()) // Converte os dados binários para string
+        .pipeThrough(
+          new TransformStream({
+            transform(chunk, controller) {
+              chunk.split('\n').forEach((line) => {
+                if (line.startsWith('data: ')) {
+                  controller.enqueue(line.replace('data: ', '').trim())
+                }
+              })
+            },
+          }),
+        )
+
+      this.dataStream = stream
+    } catch (error) {
+      console.log(error)
     }
-
-    const stream = data.body
-      ?.pipeThrough(new TextDecoderStream()) // Converte os dados binários para string
-      .pipeThrough(
-        new TransformStream({
-          transform(chunk, controller) {
-            chunk.split('\n').forEach((line) => {
-              if (line.startsWith('data: ')) {
-                controller.enqueue(line.replace('data: ', '').trim())
-              }
-            })
-          },
-        }),
-      )
-
-    this.dataStream = stream
   }
 
   onMessage<T>(cb: (data: T) => void) {
@@ -40,10 +41,15 @@ export class ProcessSEE {
       new WritableStream({
         write(data: string) {
           const parsedData = JSON.parse(data) as T
-          console.log('Mensagem SSE recebida:', parsedData)
           cb(parsedData)
         },
       }),
+      { signal: this.abortController?.signal },
     )
+  }
+
+  close() {
+    this.abortController?.abort('service closed')
+    this.abortController = null
   }
 }
