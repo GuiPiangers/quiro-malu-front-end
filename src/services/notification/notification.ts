d769c87371bc9597001e416b1dcf80669ad1b1d0
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2025-02-21 22:14:16 -0300

    feat: create type NotificationDTO

--- src/services/notification/notification.ts
+++ src/services/notification/notification.ts
@@ -2,6 +2,13 @@
 
 import { api } from '../api/api'
 
+export type NotificationDTO = {
+  message: string
+  read: boolean
+  title: string
+  type: string
+}
+
 export async function subscribeNotification(subscription: string) {
   const res = await api('/subscribe', {
     method: 'POST',
