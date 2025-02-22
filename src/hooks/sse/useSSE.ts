1507f1ce50756feb1f7cedda6b8f40a2e6a1258e
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2025-02-22 14:03:32 -0300

    feat: use ProcessSSE class to connect with backend notification SSE route

--- src/hooks/sse/useSSE.ts
+++ src/hooks/sse/useSSE.ts
@@ -1,23 +1,34 @@
+import { ProcessSEE } from '@/utils/processSSE'
 import { useEffect, useState } from 'react'
 
 export function useSSE<T>(url: string) {
   const [events, setEvents] = useState<T>()
+  const eventSource = new ProcessSEE()
 
   useEffect(() => {
-    let eventSource: EventSource | null = null
+    const tokenEntry = document.cookie
+      .split(';')
+      .map((cookieString) => {
+        return cookieString.trim().split('=')
+      })
+      .find(([key]) => key === 'quiro-token')
 
-    eventSource = new EventSource(url)
+    const token = tokenEntry ? tokenEntry[1] : undefined
 
-    eventSource.onmessage = (event) => {
-      const data = JSON.parse(event.data)
-      console.log('📩 Mensagem recebida:', data)
-      setEvents(data)
+    const connect = async () => {
+      await eventSource.connect(url, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+
+      eventSource.onMessage<T>((data) => {
+        console.log('📩 Mensagem recebida:', data)
+        setEvents(data)
+      })
     }
 
-    eventSource.onerror = () => {
-      eventSource?.close()
-      sessionStorage.removeItem('sse_active')
-    }
+    connect()
 
     return () => {
       eventSource?.close()
