ff5111423063e83365fbbc3bed60d94d2cc3d05a
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2025-03-01 12:56:50 -0300

    feat: use component NotificationTable on /notification page

--- src/app/(private)/notifications/page.tsx
+++ src/app/(private)/notifications/page.tsx
@@ -1,5 +1,5 @@
 import { Box } from '@/components/box/Box'
-import { NotificationItem } from '@/components/notification/itens/NotificationItem'
+import NotificationTable from '@/components/notification/notificationTable/NotificationTable'
 import { Table } from '@/components/table'
 import { Validate } from '@/services/api/Validate'
 import { listNotifications } from '@/services/notification/notification'
@@ -8,23 +8,15 @@ export default async function NotificationPage() {
   const notifications = await listNotifications().then((res) =>
     Validate.isOk(res) ? res : undefined,
   )
+
   return (
     <Box className="w-full max-w-screen-lg">
       <Table.Root>
         {notifications?.length === 0 && (
           <span>Você não tem nenhuma notificação!</span>
         )}
-        {notifications?.map(
-          ({ message, read, title, type, actionNeeded, id }) => (
-            <NotificationItem
-              type={type}
-              key={id}
-              notRead={read}
-              actionNeeded={actionNeeded}
-              message={message}
-              title={title}
-            />
-          ),
+        {notifications && (
+          <NotificationTable notifications={notifications}></NotificationTable>
         )}
       </Table.Root>
     </Box>
