ef782d2b1ee263ba752e9419ac7f35f3e5ecaad8
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2025-08-28 21:52:01 -0300

    feat: Adapt SchedulingList to render Scheduling and BlockScheduling events

--- src/components/schedulingList/SchedulingList.tsx
+++ src/components/schedulingList/SchedulingList.tsx
@@ -2,6 +2,8 @@
 
 import {
   SchedulingResponse,
+  SchedulingWithPatient,
+  listEvents,
   listSchedules,
 } from '@/services/scheduling/scheduling'
 import {
@@ -21,10 +23,11 @@ import StatusSelect from './StatusSelect'
 import { useQuery } from '@tanstack/react-query'
 import { SchedulingStatusEnum } from '@/services/scheduling/schedulingStatusEnum'
 import { Validate } from '@/services/api/Validate'
-import { useRef, useState } from 'react'
+import { Dispatch, SetStateAction, useRef, useState } from 'react'
 import { ModalHandles } from '../modal/Modal'
 import SchedulingModalContent from '../modal/SchedulingModal/SchedulingModalContent'
 import Phone from '@/utils/Phone'
+import { isSchedulingEvent } from '@/utils/eventValidator'
 
 type SchedulingListProps = {
   date: string
@@ -52,29 +55,22 @@ export default function SchedulingList({
   const { data } = useQuery({
     queryKey: ['listSchedules', date],
     queryFn: async () =>
-      await listSchedules({
+      await listEvents({
         date,
       }),
-    initialData: {
-      schedules,
-    },
   })
+
   const modalRef = useRef<ModalHandles>(null)
 
   const openModal = modalRef.current?.openModal
-  const [modalData, setModalData] = useState<
-    Partial<
-      SchedulingResponse & {
-        patient: string
-        patientPhone: string
-      }
-    >
-  >()
+  const [modalData, setModalData] = useState<Partial<SchedulingWithPatient>>()
 
   const generateWorkHours = new GenerateWorkHours(workHours)
   const table = generateWorkHours.generate(
-    Validate.isOk(data) ? data.schedules : schedules,
+    Validate.isOk(data) && data ? data.data : schedules,
   )
+
+  console.log(table)
 
   if (!DateTime.validateDate(date))
     throw new Error('A data informada está incorreta')
@@ -84,130 +80,52 @@ export default function SchedulingList({
       <AccordionTable.Root>
         {table.map((item) => {
           const [hour, scheduling] = item
-          if (scheduling) {
-            const durationString = new Time(
-              scheduling.duration,
-            ).getHoursAndMinutes()
-
+          if (!scheduling) {
             return (
-              <AccordionTable.Item key={scheduling.id}>
-                <AccordionTable.Row
-                  columns={['2fr', '2fr', '1fr']}
-                  data-status={scheduling.status}
-                  className={`${
-                    statusColors[scheduling.status] === 'blue' &&
-                    'text-blue-600'
-                  } ${
-                    statusColors[scheduling.status] === 'green' &&
-                    'text-green-600'
-                  } ${
-                    statusColors[scheduling.status] === 'red' && 'text-red-600'
-                  }
-                ${
-                  statusColors[scheduling.status] === 'yellow' &&
-                  'text-orange-600 opacity-60'
-                }
-                `}
+              <Button
+                key={hour}
+                className="contents text-black"
+                onClick={() => {
+                  openModal && openModal()
+                  setModalData({ date: `${date}T${hour}` })
+                }}
+              >
+                <Table.Row
+                  clickable
+                  columns={['auto', '1fr']}
+                  className="group"
                 >
-                  <AccordionTable.Cell>
-                    {hour} {'('}
-                    {durationString}
-                    {')'}
-                  </AccordionTable.Cell>
-
-                  <AccordionTable.Cell>
-                    {scheduling.patient}
-                  </AccordionTable.Cell>
-                  <StopPropagation>
-                    <AccordionTable.Cell>
-                      <StatusSelect
-                        duration={scheduling.duration}
-                        date={scheduling.date}
-                        schedulingId={scheduling.id || ''}
-                        status={scheduling.status}
-                        color={statusColors[scheduling.status]}
-                      ></StatusSelect>
-                    </AccordionTable.Cell>
-                  </StopPropagation>
-                </AccordionTable.Row>
-
-                <AccordionTable.Content className="flex justify-between gap-2">
-                  <div className="space-y-1 text-sm">
-                    <p>
-                      <strong>Paciente:</strong> {scheduling.patient}
-                    </p>
-                    <p>
-                      <strong>Serviço:</strong> {scheduling.service}
-                    </p>
-                    <p>
-                      <strong>Duração:</strong> {durationString}
-                    </p>
-                    <div className="flex gap-2 pt-2 ">
-                      <Button
-                        variant="outline"
-                        size="small"
-                        color="blue"
-                        className="w-20"
-                        onClick={() => {
-                          openModal && openModal()
-                          setModalData({
-                            ...scheduling,
-                            service: scheduling.service,
-                            patientPhone: scheduling.phone,
-                          })
-                        }}
-                      >
-                        Editar
-                      </Button>
-                      <DeleteSchedulingButton id={scheduling.id!} />
-                    </div>
-                  </div>
-                  <div className="flex flex-col gap-2">
-                    <RealizeScheduling
-                      size="small"
-                      formData={{
-                        date: scheduling.date,
-                        patientId: scheduling.patientId,
-                        schedulingId: scheduling.id!,
-                        service: scheduling.service,
-                        patient: scheduling.patient,
-                      }}
-                    >
-                      Realizar consulta
-                    </RealizeScheduling>
-                    <Button variant="outline" size="small">
-                      <Link
-                        href={`https://wa.me/55${Phone.unformat(
-                          scheduling.phone || '',
-                        )}`}
-                        target="_blank"
-                      >
-                        Contato
-                      </Link>
-                    </Button>
-                    <Button asChild variant="outline" size="small">
-                      <Link href={`/patients/${scheduling.patientId}`}>
-                        Ficha
-                      </Link>
-                    </Button>
-                  </div>
-                </AccordionTable.Content>
-              </AccordionTable.Item>
+                  <Table.Cell>{hour}</Table.Cell>
+                  <Table.Cell className="pointer-events-none w-full rounded border border-slate-300 text-center text-slate-400 opacity-0 group-hover:opacity-100">
+                    Novo Agendamento
+                  </Table.Cell>
+                </Table.Row>
+              </Button>
             )
           }
+
+          if (isSchedulingEvent(scheduling)) {
+            return (
+              <SchedulingTableItem
+                scheduling={scheduling}
+                hour={hour}
+                key={scheduling.id}
+                setModalData={setModalData}
+                openModal={openModal}
+              />
+            )
+          }
+
           return (
-            <Button
-              key={hour}
-              className="contents text-black"
-              onClick={() => {
-                openModal && openModal()
-                setModalData({ date: `${date}T${hour}` })
-              }}
-            >
-              <Table.Row clickable columns={['auto', '1fr']} className="group">
+            <Button key={hour} className="contents text-slate-500">
+              <Table.Row
+                clickable
+                columns={['auto', '1fr']}
+                className="group bg-slate-200"
+              >
                 <Table.Cell>{hour}</Table.Cell>
-                <Table.Cell className="pointer-events-none w-full rounded border border-slate-300 text-center text-slate-400 opacity-0 group-hover:opacity-100">
-                  Novo Agendamento
+                <Table.Cell className="w-full">
+                  {scheduling.description}
                 </Table.Cell>
               </Table.Row>
             </Button>
@@ -221,5 +139,115 @@ export default function SchedulingList({
         formData={modalData}
       ></SchedulingModalContent>
     </>
+  )
+}
+
+function SchedulingTableItem({
+  hour,
+  scheduling,
+  setModalData,
+  openModal,
+}: {
+  scheduling: SchedulingWithPatient
+  hour: string
+  setModalData: Dispatch<
+    SetStateAction<Partial<SchedulingWithPatient> | undefined>
+  >
+  openModal?: () => void
+}) {
+  const durationString = new Time(scheduling.duration).getHoursAndMinutes()
+  return (
+    <AccordionTable.Item key={scheduling.id}>
+      <AccordionTable.Row
+        columns={['2fr', '2fr', '1fr']}
+        data-status={scheduling.status}
+        className={`${
+          statusColors[scheduling.status] === 'blue' && 'text-blue-600'
+        } ${statusColors[scheduling.status] === 'green' && 'text-green-600'} ${
+          statusColors[scheduling.status] === 'red' && 'text-red-600'
+        }
+      ${
+        statusColors[scheduling.status] === 'yellow' &&
+        'text-orange-600 opacity-60'
+      }
+      `}
+      >
+        <AccordionTable.Cell>
+          {hour} {'('}
+          {durationString}
+          {')'}
+        </AccordionTable.Cell>
+
+        <AccordionTable.Cell>{scheduling.patient}</AccordionTable.Cell>
+        <StopPropagation>
+          <AccordionTable.Cell>
+            <StatusSelect
+              duration={scheduling.duration}
+              date={scheduling.date}
+              schedulingId={scheduling.id || ''}
+              status={scheduling.status}
+              color={statusColors[scheduling.status]}
+            ></StatusSelect>
+          </AccordionTable.Cell>
+        </StopPropagation>
+      </AccordionTable.Row>
+
+      <AccordionTable.Content className="flex justify-between gap-2">
+        <div className="space-y-1 text-sm">
+          <p>
+            <strong>Paciente:</strong> {scheduling.patient}
+          </p>
+          <p>
+            <strong>Serviço:</strong> {scheduling.service}
+          </p>
+          <p>
+            <strong>Duração:</strong> {durationString}
+          </p>
+          <div className="flex gap-2 pt-2 ">
+            <Button
+              variant="outline"
+              size="small"
+              color="blue"
+              className="w-20"
+              onClick={() => {
+                openModal && openModal()
+                setModalData({
+                  ...scheduling,
+                  service: scheduling.service,
+                })
+              }}
+            >
+              Editar
+            </Button>
+            <DeleteSchedulingButton id={scheduling.id!} />
+          </div>
+        </div>
+        <div className="flex flex-col gap-2">
+          <RealizeScheduling
+            size="small"
+            formData={{
+              date: scheduling.date,
+              patientId: scheduling.patientId,
+              schedulingId: scheduling.id!,
+              service: scheduling.service,
+              patient: scheduling.patient,
+            }}
+          >
+            Realizar consulta
+          </RealizeScheduling>
+          <Button variant="outline" size="small">
+            <Link
+              href={`https://wa.me/55${Phone.unformat(scheduling.phone || '')}`}
+              target="_blank"
+            >
+              Contato
+            </Link>
+          </Button>
+          <Button asChild variant="outline" size="small">
+            <Link href={`/patients/${scheduling.patientId}`}>Ficha</Link>
+          </Button>
+        </div>
+      </AccordionTable.Content>
+    </AccordionTable.Item>
   )
 }
