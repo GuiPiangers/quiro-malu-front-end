c87d91fa1949a1203c896d4455ad887ecf9f4a01
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2025-09-10 09:06:18 -0300

    feat: Buscando a configuração do calendário na CalendarPage

--- src/app/(private)/config/calendario/page.tsx
+++ src/app/(private)/config/calendario/page.tsx
@@ -1,5 +1,11 @@
 import CalendarSettingsForm from '@/components/form/calendar/CalendarSettingsForm'
+import { Validate } from '@/services/api/Validate'
+import { getCalendarConfiguration } from '@/services/config/calendarConfiguration'
 
-export default function CalendarPage() {
-  return <CalendarSettingsForm />
+export default async function CalendarPage() {
+  const calendarConfiguration = await getCalendarConfiguration().then((res) =>
+    Validate.isError(res) ? undefined : res,
+  )
+
+  return <CalendarSettingsForm formData={calendarConfiguration} />
 }
