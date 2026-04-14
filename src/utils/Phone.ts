83f045a607c7bcce26d015602756b06c62ca1fca
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2026-04-14 15:31:42 -0300

    feat: Add international phone number formatting method to Phone utility

--- src/utils/Phone.ts
+++ src/utils/Phone.ts
@@ -2,6 +2,7 @@ import { removeNotNumbers } from './removeNotNumbers'
 
 export default class Phone {
   private static _pattern = '(??) ????? ????'
+  private static _internationalPattern = '+?? (??) ????? ????'
 
   static format(value: string): string {
     const nums = Phone.unformat(value).split('')
@@ -12,6 +13,18 @@ export default class Phone {
       .split('?')[0]
       .trim()
       .replace(/[() ]$/, '')
+  }
+
+  static internationalFormat(value: string): string {
+    const nums = Phone.unformat(value).split('')
+    if (nums.length === 0) return ''
+    return nums
+      .reduce((formatted: string, num: string) => {
+        return formatted.replace('?', num)
+      }, Phone._internationalPattern)
+      .split('?')[0]
+      .trim()
+      .replace(/[()+ ]+$/g, '')
   }
 
   static unformat(value: string): string {
